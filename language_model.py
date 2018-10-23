4679f985e285c5e230b4e5cb323deb364960d6b9
Ali Gholami <aligholami7596@gmail.com>
2018-10-23 08:29:37 +0330

    Update: Get one_hot word vectors

--- language_model.py
+++ language_model.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import numpy as np
+
+np.set_printoptions(threshold=np.nan)
+
 def get_num_words(sequence):
 
     num_words = 0
@@ -24,22 +27,27 @@ def get_word_vocabulary(sequence):
     
     return word_vocab
 
-def get_ix_to_char(vocab):
+def get_ix_to_word(vocab):
 
-    ix_to_char = {ix:ch  for ix, ch in enumerate(vocab)}
-    
-    return ix_to_char
+    ix_to_word = {ix:word  for ix, word in enumerate(vocab)}
 
-def get_char_to_ix(vocab):
+    return ix_to_word
 
-    char_to_ix = {ch:ix for ix, ch in enumerate(vocab)} 
+def get_word_to_ix(vocab):
 
-    return char_to_ix
+    word_to_ix = {word:ix for ix, word in enumerate(vocab)} 
+
+    return word_to_ix
+
 
 def get_vector(word, vocab):
 
     word_one_hot_vec = np.zeros(shape=(len(vocab), 1))
+    for ix, _word in enumerate(vocab):
+        if _word == word:
+            word_one_hot_vec[ix] = 1
 
+    return word_one_hot_vec
 
 BATCH_SIZE = 4
 NUM_FEATURES = 5
@@ -49,10 +57,7 @@ train_data = open('ptb.train.txt', 'r').read()
 train_data_num_words = get_num_words(train_data)
 
 word_vocab = get_word_vocabulary(train_data)
-
-for ix, ch in enumerate(word_vocab):
-    print(ix, ":", ch)
-
+one_hot_word = get_vector('they', word_vocab)
 
 x = tf.placeholder(dtype=tf.float32, shape=[TIME_STEPS, BATCH_SIZE, NUM_FEATURES], name='RNN_Input')
 
