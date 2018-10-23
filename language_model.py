5997c88745670145a37ec6dc1cda27cb9b163289
Ali Gholami <aligholami7596@gmail.com>
2018-10-23 11:53:56 +0330

    Update: changed corpus

--- language_model.py
+++ language_model.py
@@ -42,27 +42,29 @@ def get_word_to_ix(vocab):
 
 def get_vector(word, vocab):
 
-    word_one_hot_vec = np.zeros(shape=(len(vocab), 1))
+    word_one_hot_vec = np.zeros(shape=(len(vocab)))
     for ix, _word in enumerate(vocab):
         if _word == word:
             word_one_hot_vec[ix] = 1
 
     return word_one_hot_vec
 
-def corpus_to_vec(text_corpus, batch_size, time_steps):
+def corpus_to_vec(text_corpus, time_steps):
 
     word_vocab = get_word_vocabulary(text_corpus)
     word_vocab_size = len(word_vocab)
 
-    wordlist = sequence.split(' ')
-    num_vectors = len(wordlist) / time_steps
+    wordlist = text_corpus.split(' ')
+    num_vectors = int(len(wordlist) / time_steps)
 
     # Our word vocab size will be similar to our one hot vectors in terms of dims
-    vectorized_corpus = np.zeros(shape=(time_steps, batch_size, word_vocab_size))
-    vectorized_labels = np.zeros(shape=(time_steps, batch_size, word_vocab_size))
+    vectorized_corpus = np.zeros(shape=(time_steps, num_vectors, word_vocab_size))
+    vectorized_labels = np.zeros(shape=(time_steps, num_vectors, word_vocab_size))
 
-    for vec_idx in num_vectors:
-        for step in time_steps:
+    print("Number of vectors: {0}".format(num_vectors))
+
+    for vec_idx in range(num_vectors):
+        for step in range(time_steps):
             vectorized_corpus[step:vec_idx:] = get_vector(wordlist[vec_idx * time_steps + step], word_vocab)
             vectorized_labels[step:vec_idx:] = get_vector(wordlist[vec_idx * time_steps + step + 1], word_vocab)
    
@@ -72,6 +74,8 @@ def corpus_to_vec(text_corpus, batch_size, time_steps):
 # Load and preprocess the data
 text_corpus = open('ptb.train.txt', 'r').read()
 num_corpus_words = get_num_words(text_corpus)
+word_vocab = get_word_vocabulary(text_corpus)
+word_vocab_size = len(word_vocab)
 
 # No dense embeddings -> one-hot vectors only
 # Hyperparams
@@ -83,37 +87,37 @@ lstm_size = 128
 learning_rate = 0.001
 
 # Get array for each word in the corpus and place them in 10 timesteps formats
-x_train, y_train = corpus_to_vec(text_corpus, batch_size, time_steps)
+x_train, y_train = corpus_to_vec(text_corpus, time_steps)
 
-#############################
-# Beginning of the TF Graph #
-#############################
-x = tf.placeholder(dtype=tf.float32, shape=[time_steps, batch_size, num_features], name='RNN_Input')
-y = tf.placeholder(dtype=tf.float32, shape=[-1])
+# #############################
+# # Beginning of the TF Graph #
+# #############################
+# x = tf.placeholder(dtype=tf.float32, shape=[time_steps, batch_size, num_features], name='RNN_Input')
+# y = tf.placeholder(dtype=tf.float32, shape=[-1])
 
-# Define the model
-lstm = tf.nn.rnn_cell.LSTMCell(num_units=lstm_size, dtype=tf.float32)
+# # Define the model
+# lstm = tf.nn.rnn_cell.LSTMCell(num_units=lstm_size, dtype=tf.float32)
 
-# Initial LSTM memory
-z_state = lstm.zero_state(batch_size=batch_size, dtype=tf.float32)
+# # Initial LSTM memory
+# z_state = lstm.zero_state(batch_size=batch_size, dtype=tf.float32)
 
-output, state = lstm(inputs=word_batch, state=z_state)
+# output, state = lstm(inputs=word_batch, state=z_state)
 
-# Final dense layer
-logits = tf.layers.dense(inputs=output, units=num_features, activation=None, use_bias=True)
+# # Final dense layer
+# logits = tf.layers.dense(inputs=output, units=num_features, activation=None, use_bias=True)
 
-# Loss definition
-loss = tf.nn.softmax_cross_entropy_with_logits(features=logits, labels=y)
+# # Loss definition
+# loss = tf.nn.softmax_cross_entropy_with_logits(features=logits, labels=y)
 
-# Optimizer
-opt = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
+# # Optimizer
+# opt = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
 
-with tf.Session() as sess:
+# with tf.Session() as sess:
 
-    total_loss = 0
-    for epoch in num_epochs:
+#     total_loss = 0
+#     for epoch in num_epochs:
         
-        preds, loss, _ = sess.run([logits, loss, opt], feed_dict={x:???, y:???})
+#         preds, loss, _ = sess.run([logits, loss, opt], feed_dict={x:???, y:???})
 
 
 
