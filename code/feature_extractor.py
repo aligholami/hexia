43c5f52c1c488fb3825b9bb462fbe36e55ccc3ed
Ali Gholami <aligholamee@aut.ac.ir>
2018-12-16 12:58:41 +0330

    Update: Added a simple 3 layer CNN

--- code/feature_extractor.py
+++ code/feature_extractor.py
@@ -2,11 +2,53 @@ import tensorflow as tf
 
 class FeatureExtractor:
 
-    def __init__():
-        pass;
+    def __init__(self, keep_prob, flatten=True):
         
+        self.keep_prob = keep_prob
+        self.flatten = flatten
+
     def assemble_cnn_model(self, cnn_input):
-        
+
+        conv1 = self.conv_bn_sc_relu(
+            inputs=cnn_input,
+            filters=CONV1_NUM_FILTERS,
+            k_size=CONV1_FILTER_SIZE,
+            stride=1,
+            padding='SAME',
+            scope_name='conv_1',
+            keep_prob=self.keep_prob
+        )
+    
+        conv2 = self.conv_bn_sc_relu(
+            inputs=conv1,
+            filters=CONV2_NUM_FILTERS,
+            k_size=CONV2_FILTER_SIZE,
+            stride=1,
+            padding='SAME',
+            scope_name='conv_2',
+            keep_prob=self.keep_prob
+        )
+
+        conv3 = self.conv_bn_sc_relu(
+            inputs=conv2,
+            filters=CONV3_NUM_FILTERS,
+            k_size=CONV3_FILTER_SIZE,
+            stride=1,
+            padding='SAME',
+            scope_name='conv_3',
+            keep_prob=self.keep_prob
+        )
+
+        if(self.flatten == False):
+            return conv3
+
+        else:
+            flattened = tf.layers.flatten(
+                inputs=conv3,
+                name='flatten_input'
+            )
+
+            return flattened
     
     def conv_bn_sc_relu(self, inputs, filters, k_size, stride, padding, scope_name, keep_prob):
 
@@ -48,7 +90,7 @@ class FeatureExtractor:
 
         return scaled_input
 
-    def maxpool(inputs, k_size, stride, padding, scope_name):
+    def maxpool(self, inputs, k_size, stride, padding, scope_name):
 
         with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
             pool = tf.nn.max_pool(value=inputs, ksize=[1, k_size, k_size, 1], strides=[1, stride, stride, 1], padding=padding)
