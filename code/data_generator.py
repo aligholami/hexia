7123a6a963d18b7a4871de01e898a33ae8aa761a
Ali Gholami <aligholamee@aut.ac.ir>
2018-12-31 13:50:31 +0330

    Update: Added a new implementation for mini-batch generator

--- code/data_generator.py
+++ code/data_generator.py
@@ -2,6 +2,7 @@ import cv2
 from utils import get_image_id
 import json
 import os
+from math import ceil
 
 # from text_generator import TextGenerator
 
@@ -32,13 +33,18 @@ class DataGenerator:
     def confidence_to_one_hot(self, confidence_list):
         
         # confidences for (yes) / (maybe, no)
-        one_hot_confidences = [0, 0]
-
-        one_hot_confidences = [if x is 'yes' [1, 0] else [0, 1] for x in confidence_list]
-
+        one_hot_confidences = []
+        for x in confidence_list:
+            if x == 'yes':
+                one_hot_confidences.append([1, 0, 0])
+            elif x == 'maybe':
+                one_hot_confidence.append([0, 1, 0])
+            else:
+                one_hot_confidence.append([0, 0, 1])
+                
         return one_hot_confidences
 
-    def mini_batch_generator(self, batch_size):
+    def mini_batch_generator(self):
         
         # Generate a batch of images
         # for each image in the batch generate 
@@ -52,28 +58,24 @@ class DataGenerator:
             # Extract the image ID
             img_id = get_image_id(image_name)
 
-            # Extract questions and answers from the JSON files
+            # Extract questions, answers, and labels (confidences) from the JSON files
             img_question_list = []
             img_answer_list = []
             iqa_label_list = []
 
-            for entry in self.q_data['questions']:
-                if(entry['image_id'] == int(img_id)):
-                    img_question_list.append(entry['question'])
-            
-            for entry in self.a_data['annotations']:
-                if(entry['image_id'] == int(img_id)):
-                    for i in entry['answers']:
-                        img_answer_list.append(entry['answers'][i]['answer'])
-                        iqa_label_list.append(entry['answers'][i]['confidence'])
-            
+            for question in self.q_data['questions']:
+                if(question['image_id'] == int(img_id)):
+                    for annotation in self.a_data['annotations']:
+                        if(annotation['question_id'] == question['question_id']):
+                            answer_no = 0
+                            for answer in annotation['answers']:
+                                batch_item = {}
+                                batch_item['image'] = img
+                                batch_item['question'] = question['question']
+                                batch_item['answer'] = annotation['answers'][answer_no]['answer']
+                                batch_item['iqa_label'] = annotation['answers'][answer_no]['answer_confidence']
+                                answer_no = answer_no + 1
+                                yield (batch_item['image'], batch_item['question'], batch_item['answer'], batch_item['iqa_label'])
+                    
             # Grab one-hot vectors for the confidences list
-            one_hot_iqa = confidence_to_one_hot(iqa_label_list)
-
-            for item in range(len(iqa_label_list)):
-                batch_item = {}
-                batch_item['image'] = img
-                batch_item['question'] = img_question_list[item]
-                batch_item['answer'] = img_answer_list[item]
-                batch_item['iqa_label'] = one_hot_iqa[item]     # one-hot labels
-                yield (batch_item['image'], batch_item['question'], batch_item['answer'], batch_item['iqa_label'])
+            # one_hot_iqa = confidence_to_one_hot(iqa_label_list)
--- code/main.py
+++ code/main.py
@@ -1,9 +1,17 @@
 from vqa_san import VQA_SAN
+from data_generator import DataGenerator
 
-vqa_model = VQA_SAN()
+# vqa_model = VQA_SAN()
+
+gen = DataGenerator(image_path='../data/train/images/full-image-dir', q_path='../data/train/questions/v2_OpenEnded_mscoco_train2014_questions.json', a_path='../data/train/answers/v2_mscoco_train2014_annotations.json', image_rescale=1, image_horizontal_flip=False, image_target_size=(150, 150))
+
+mb_gen = gen.mini_batch_generator()
+
+for i in range(20):
+    print(mb_gen.__next__())
 
 
-data_gen = vqa_model.get_data()
+# data_gen = vqa_model.get_data()
 
-for i in range(10):
-    print(data_gen.__next__())
+# for i in range(10):
+#     print(data_gen.__next__())
