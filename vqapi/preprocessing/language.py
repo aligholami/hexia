508f4c6ab05baa425a7528034d71ef0df9d673d5
Ali Gholami <aligholami7596@gmail.com>
2019-04-14 17:54:55 +0430

    Update: save vocab to dir

--- vqapi/preprocessing/language.py
+++ vqapi/preprocessing/language.py
@@ -1,10 +1,14 @@
 import itertools
+import json
 from collections import Counter
+from vqapi.backend.utilities import utils
+from vqapi.backend.dataset import data
 
 class Language:
 
-    def __init__(self):
-        pass
+    def __init__(self, max_answers, save_vocab_to):
+        self.max_answers = max_answers
+        self.save_vocab_to = save_vocab_to
 
     def extract_vocab(self, iterable, top_k=None, start=0):
         """ 
@@ -27,5 +31,31 @@ class Language:
         vocab = {t: i for i, t in enumerate(tokens, start=start)}
 
         return vocab
+
+    def initiate_vocab_extraction(self):
+        questions = utils.path_for(train=True, question=True)
+        answers = utils.path_for(train=True, question=True)
+
+        with open(questions, 'r') as fd:
+            questions = json.load(fd)
+        with open(answers, 'r') as fd:
+            answers = json.load(fd)
+
+        questions = data.prepare_questions(questions)
+        answers = data.prepare_answers(answers)
+
+        question_vocab = self.extract_vocab(questions, start=1)
+        answer_vocab = self.extract_vocab(answers, top_k=self.max_answers)
+
+        vocabs = {
+            'question': question_vocab,
+            'answer': answer_vocab
+        }
+
+        with open(self.save_vocab_to, 'w') as fd:
+            json.dump(vocabs, fd)
+
+        
+
 
     
