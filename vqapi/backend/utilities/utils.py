a35bb676892fd4d767e723e5f5cb0db29f7d9140
Ali Gholami <aligholami7596@gmail.com>
2019-04-16 17:18:50 +0430

    Update: Remove duplicates

--- vqapi/backend/utilities/utils.py
+++ vqapi/backend/utilities/utils.py
@@ -10,9 +10,6 @@ from tqdm import tqdm
 from vqapi.tests import config
 from vqapi.backend.dataset.data import DataLoadUtils
 
-train_iters = 0
-val_iters = 0
-
 def reload_dataset_vocab():
     """
     Reloads VQA V2 dataset vocabulary into memory to be used by models (Text Processor).
@@ -116,85 +113,6 @@ def save_for_vqa_evaluation(anws, ids, qids):
 
     with open(pth, 'w') as eFile:
         json.dump(evaluation_list, eFile)
-
-def run(net, loader, optimizer, tracker, writer, train=False, prefix='', epoch=0):
-    """ Run an epoch over the given loader """
-    if train:
-        net.train()
-        tracker_class, tracker_params = tracker.MovingMeanMonitor, {'momentum': 0.99}
-    else:
-        net.eval()
-        tracker_class, tracker_params = tracker.MeanMonitor, {}
-        answ = []
-        idxs = []
-        accs = []
-        qids = []
-
-    tq = tqdm(loader, desc='{} E{:03d}'.format(prefix, epoch), ncols=0)
-    loss_tracker = tracker.track('{}_loss'.format(prefix), tracker_class(**tracker_params))
-    acc_tracker = tracker.track('{}_acc'.format(prefix), tracker_class(**tracker_params))
-
-    log_softmax = nn.LogSoftmax().cuda()
-    for qid, v, q, a, idx, q_lens in tq:
-        var_params = {
-            'volatile': not train,
-            'requires_grad': False,
-        }
-        v = Variable(v.cuda(async=True), **var_params)
-        q = Variable(q.cuda(async=True), **var_params)
-        a = Variable(a.cuda(async=True), **var_params)
-
-        # used for sequence padding and packing
-        q_lens = Variable(q_lens.cuda(async=True), **var_params)
-
-        out = net(v, q, q_lens)
-        nll = -log_softmax(out)
-        loss = (nll * a / 10).sum(dim=1).mean()
-        acc = batch_accuracy(out.data, a.data).cpu()
-
-        if train:
-            global train_iters
-            update_learning_rate(optimizer, train_iters)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            train_iters += 1
-            # Write loss and accuracy to TensorboardX
-            writer.add_scalar('/loss', loss.data.item(), train_iters)
-            writer.add_scalar('/accuracy', acc.mean(), train_iters)
-
-        else:
-            global val_iters
-            # store information about evaluation of this minibatch
-            _, answer = out.data.cpu().max(dim=1)
-            qids.append(qid.view(-1).clone())
-            answ.append(answer.view(-1))
-            accs.append(acc.view(-1))
-            idxs.append(idx.view(-1).clone())
-            val_iters += 1
-
-            # Write loss and accuracy to TensorboardX
-            writer.add_scalar('/loss', loss.data.item(), val_iters)
-            writer.add_scalar('/accuracy', acc.mean(), val_iters)
-
-        loss_tracker.append(loss.data.item())
-        acc_tracker.append(acc.mean())
-        fmt = '{:.4f}'.format
-        tq.set_postfix(loss=fmt(loss_tracker.mean.value), acc=fmt(acc_tracker.mean.value))
-
-    if not train:
-        answ = list(torch.cat(answ, dim=0))
-        accs = list(torch.cat(accs, dim=0))
-        idxs = list(torch.cat(idxs, dim=0))
-        qids = list(torch.cat(qids, dim=0))
-
-        # Save results for vqa evaluation
-        save_for_vqa_evaluation(answ, idxs, qids, epoch)
-
-        return answ, accs, idxs
-
 
 def path_for(train=False, val=False, test=False, question=False, answer=False):
     assert train + val + test == 1
